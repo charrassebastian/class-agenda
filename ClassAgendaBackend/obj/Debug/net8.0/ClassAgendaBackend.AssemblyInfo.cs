abecbe70be62c36a06062905f37ed9430bded909
Sebastian Horacio Charras <sebastiancharras@outlook.com>
2024-01-08 15:40:30 -0300

    .

--- ClassAgendaBackend/obj/Debug/net8.0/ClassAgendaBackend.AssemblyInfo.cs
+++ ClassAgendaBackend/obj/Debug/net8.0/ClassAgendaBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassAgendaBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac0f62dab42d3806a0264db944d54f40b85b7aae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc39bd4642b808951c0cbe20c8a9c6a6aab11ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassAgendaBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassAgendaBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
